3edf3d4da0ad096f9a016fdc80d076f876f195b8
FrankDestro <frank-destro@outlook.com>
2024-06-12 04:17:29 -0300

    feat: implementação e configuração do Eureka Client
    - incluido as dependencias do eureka client nos microserviços hr-payroll e hr-worker
    - incluido annotation nas classes principais dos microserviços hr-payroll e hr-worker
    - efetuado configuração da localização do eureka server nos arquivos application.properties dos microserviços hr-payroll e hr-worker

--- hr-payroll/src/main/java/com/devsuperior/hrpayroll/HrPayrollApplication.java
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/HrPayrollApplication.java
@@ -3,11 +3,11 @@ package com.devsuperior.hrpayroll;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.openfeign.FeignAutoConfiguration;
 
-@RibbonClient(name = "hr-worker")
+@EnableDiscoveryClient
 @EnableFeignClients
 @SpringBootApplication
 @ImportAutoConfiguration({FeignAutoConfiguration.class})
