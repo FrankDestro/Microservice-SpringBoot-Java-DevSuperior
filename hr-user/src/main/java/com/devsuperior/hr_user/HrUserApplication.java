f48186924e90c43524fa4ebc367192951886082a
frank <frank-destro@outlook.com>
2024-06-17 12:55:34 -0300

    feat : seeding inicial database - efetuado seeding inical no banco de dados h2 do hr-user. - implementado o ByCrypt.

--- hr-user/src/main/java/com/devsuperior/hr_user/HrUserApplication.java
+++ hr-user/src/main/java/com/devsuperior/hr_user/HrUserApplication.java
@@ -1,15 +1,16 @@
 package com.devsuperior.hr_user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableDiscoveryClient
 @SpringBootApplication
 public class HrUserApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(HrUserApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(HrUserApplication.class, args);
+    }
 }
---
+++ hr-user/src/main/java/com/devsuperior/hr_user/models/entities/Role.java
@@ -0,0 +1,26 @@
+package com.devsuperior.hr_user.models.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+@Entity
+@Table(name = "tb_role")
+public class Role implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String authority;
+
+
+}
